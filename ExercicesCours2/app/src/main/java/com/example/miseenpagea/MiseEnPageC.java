76fdfd0aa79485f102777599379a81e773cb9f7a
Félix Désilets <2259475@cegepmontpetit.ca>
2024-01-26 16:56:02 -0500

    Cours 2
    Exercices sur les mises en page.

---
+++ ExercicesCours2/app/src/main/java/com/example/miseenpagea/MiseEnPageC.java
@@ -0,0 +1,14 @@
+package com.example.miseenpagea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MiseEnPageC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
